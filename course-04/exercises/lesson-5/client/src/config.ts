ce985d1b92ccb87b1a248e3e4d9ffaec3893a28f
Markus Loeffler <m_loeffler@gmx.de>
2023-02-28 12:59:36 +0000

    lesson 4&5, audit fix lesson 6

--- course-04/exercises/lesson-5/client/src/config.ts
+++ course-04/exercises/lesson-5/client/src/config.ts
@@ -1,7 +1,8 @@
-export const apiEndpoint = '...'
+export const apiEndpoint = 'https://uo4igx3qw6.execute-api.eu-central-1.amazonaws.com/dev'
 
 export const authConfig = {
-  domain: '...',
-  clientId: '...',
+  domain: 'dev-b5l8pxg7z3snujdy.us.auth0.com',    // Name of the Auth0 domain
+  //domain: 'test-endpoint.auth0.com',    // Name of the Auth0 domain
+  clientId: 'SPNsJu7IJM62PYoT8pgCAaWCl31Z3vrH',  // Client id of a new application
   callbackUrl: 'http://localhost:3000/callback'
 }
--- course-04/exercises/lesson-5/starter-code/src/lambda/auth/auth0Authorizer.ts
+++ course-04/exercises/lesson-5/starter-code/src/lambda/auth/auth0Authorizer.ts
@@ -1,19 +1,33 @@
 import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
 import 'source-map-support/register'
-import * as middy from 'middy'
-import { secretsManager } from 'middy/middlewares'
 
 import { verify } from 'jsonwebtoken'
 import { JwtToken } from '../../auth/JwtToken'
 
-const secretId = process.env.AUTH_0_SECRET_ID
-const secretField = process.env.AUTH_0_SECRET_FIELD
+const cert = `-----BEGIN CERTIFICATE-----
+MIIDHTCCAgWgAwIBAgIJW6MbAXfSSlqmMA0GCSqGSIb3DQEBCwUAMCwxKjAoBgNV
+BAMTIWRldi1iNWw4cHhnN3ozc251amR5LnVzLmF1dGgwLmNvbTAeFw0yMzAyMjcx
+NjM2MjZaFw0zNjExMDUxNjM2MjZaMCwxKjAoBgNVBAMTIWRldi1iNWw4cHhnN3oz
+c251amR5LnVzLmF1dGgwLmNvbTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoC
+ggEBANZ2EN1HuJJIXjTVQsjWBBn3ZnBTnxM588whOPvprC0b6FFX6WyU+QdXEXWB
+m1egSFpKQgMq3T7EPIvXLd8fPUBNJUUvQcBYypE9cMgOGuFScmVCEQVxFtexSe7i
+ZkS1tntjFtmHFq/Y+akHOKxQqJDMB+asCtVQ1CMaZ+pVNwghn/EnSgpgwJYtIeef
+4fllNKFPmpy7hK0dokS7JavfWr/VvJHd4TVfAQ9JC8uV2Zb4yHHjB233C383xX9X
+sCQlyLgXMQkmkDWDCnux2vz+nmVsrjME3bDWT7plGEsS35H+7zRJRFVv5RKHtZC+
+EZrrnFs2qE+WmTQQmm8+TygwepkCAwEAAaNCMEAwDwYDVR0TAQH/BAUwAwEB/zAd
+BgNVHQ4EFgQUSrTr6sECtyUviT8lXtofseZnyYgwDgYDVR0PAQH/BAQDAgKEMA0G
+CSqGSIb3DQEBCwUAA4IBAQA+IT3uvquT7pxR/81G5DmGh+gHfB46+VaqLMw1We+M
+id+c+VrTMrC1FJF0zFFtZQGwal7T2KledJPJxKmeTWn8AYyzxI8d8+fezVXp8sh5
+WfwNiY1CJapAA/xZ8FF26XtBaEOXorDidgwqVVQiE7XXL+18hWGsDVyDVUrZs7xZ
+LL3UEx5Yyp2XZgtSEOIctKQP/eJk07Qy7/nYlV45tWS5Kv2pVDxK9KvUO069umQP
+ARqhDPxEB3SyPNY+iD0w0fDF2d4RPOgG9MH1tBmLz5kljNaoGBkwoW5rdbXkaOlv
+cN5ohq2dBgPBpxCsJ7ibUL6dBVz16nWbRzlDQ1w0uv6a
+-----END CERTIFICATE-----`
 
-export const handler = middy(async (event: CustomAuthorizerEvent, context): Promise<CustomAuthorizerResult> => {
+export const handler = async (event: CustomAuthorizerEvent): Promise<CustomAuthorizerResult> => {
   try {
     const decodedToken = verifyToken(
-      event.authorizationToken,
-      context.AUTH0_SECRET[secretField]
+      event.authorizationToken
     )
     console.log('User was authorized', decodedToken)
 
@@ -47,9 +61,9 @@ export const handler = middy(async (event: CustomAuthorizerEvent, context): Prom
       }
     }
   }
-})
+}
 
-function verifyToken(authHeader: string, secret: string): JwtToken {
+function verifyToken(authHeader: string): JwtToken {
   if (!authHeader)
     throw new Error('No authentication header')
 
@@ -59,17 +73,6 @@ function verifyToken(authHeader: string, secret: string): JwtToken {
   const split = authHeader.split(' ')
   const token = split[1]
 
-  return verify(token, secret) as JwtToken
+  return verify(token, cert, { algorithms: ['RS256'] }) as JwtToken
 }
 
-handler.use(
-  secretsManager({
-    cache: true,
-    cacheExpiryInMillis: 60000,
-    // Throw an error if can't read the secret
-    throwOnFailedCall: true,
-    secrets: {
-      AUTH0_SECRET: secretId
-    }
-  })
-)
